09a9a253392eadc5b88bbf9d2a13385fbdbd9c51
Wallace Sun <miao17game@qq.com>
2017-10-03 10:22:40 +0800

    add empty class

---
+++ WeiboOAuth2.Provider/Src/WeiboOAuth2.Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeiboOAuth2.Provider.Src {
+
+    /// <summary>
+    /// Weibo oauth 2.0 extensions.
+    /// </summary>
+    public static class WeiboOAuth2Extensions {
+
+    }
+
+}
---
+++ WeiboOAuth2.Provider/Src/WeiboOAuth2.Middleware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeiboOAuth2.Provider.Src {
+
+    /// <summary>
+    /// The middleware of weibo oauth 2.0
+    /// </summary>
+    class WeiboOAuth2Middleware {
+
+
+
+    }
+
+}
